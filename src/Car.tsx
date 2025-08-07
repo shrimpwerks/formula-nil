c65040c688648b233513a3111942d7b605db8345
Jonah George <jonah.george@icloud.com>
2025-08-07 01:16:28 -0700

    Cleanup math

--- src/Car.tsx
+++ src/Car.tsx
@@ -33,26 +33,75 @@ export function newCar(id: number, driver: Driver, startingPos: Vector2D): Car {
   const normalizedSpeed = (rawMaxSpeed - minSpeed) / speedRange;
 
   // Invert normalizedSpeed to compute accelFactor (higher speed → lower accel)
-  const BASE_ACCEL_FACTOR = 0.75; // max accel factor for slowest car
+  const BASE_ACCEL_FACTOR = 0.0005; // max accel factor for slowest car
   const accelFactor = BASE_ACCEL_FACTOR * (1 - normalizedSpeed);
+
+  const dragFactor = 0.8 * Math.random();
+  const brakeForce = 0.05 + Math.random();
 
   return {
     id: id,
     name: driver.name,
-    acceleration: new Vector2D(0, 0),
-    velocity: new Vector2D(0, 0),
+    acceleration: Vector2D.zero(),
+    velocity: Vector2D.zero(),
     color: randomColor(),
-    pos: new Vector2D(startingPos.x, startingPos.y),
+    pos: startingPos,
     accelFactor: accelFactor,
     maxSpeed: rawMaxSpeed,
-    dragFactor: 0.8 + Math.random() * 0.4,
-    brakeForce: 0.5 + Math.random() * 0.5,
+    dragFactor: dragFactor,
+    brakeForce: brakeForce,
     segmentIdx: 1,
     distanceTraveled: 0,
   };
 }
 
-export function selectCarTarget(
+function updateCarWithTarget(
+  car: Car,
+  target: Vector2D,
+  distanceToTarget: number,
+  nextSegmentIdx: number,
+): Car {
+  const directionVector = target.subtract(car.pos);
+  const trackDir = directionVector.normalized();
+
+  const velMag = car.velocity.length();
+  const velDir = velMag === 0 ? trackDir : car.velocity.normalized();
+  const alignmentDot = velDir.dot(trackDir);
+  const penalty = Math.max(0, 1 - alignmentDot);
+
+  // TODO: This should factor in optimal speed for turn
+  const slowdownDistance = car.brakeForce;
+  const slowdownFactor =
+    distanceToTarget < slowdownDistance
+      ? distanceToTarget / slowdownDistance
+      : 1;
+
+  const accel = (car.accelFactor ?? 1) * (1 - 0.5 * penalty) * slowdownFactor;
+  const acceleration = trackDir.multiplyScalar(accel);
+
+  const STEERING_GAIN = 0.2;
+  let newVel = car.velocity.add(acceleration);
+  newVel.x += (trackDir.x * velMag - newVel.x) * STEERING_GAIN;
+  newVel.y += (trackDir.y * velMag - newVel.y) * STEERING_GAIN;
+  newVel.x -= newVel.x * car.dragFactor * penalty;
+  newVel.y -= newVel.y * car.dragFactor * penalty;
+  newVel = newVel.limitLength(car.maxSpeed);
+
+  const newPos = car.pos.add(newVel);
+  const distanceThisFrame = car.pos.distanceTo(newPos);
+  const newDistanceTraveled = car.distanceTraveled + distanceThisFrame;
+
+  return {
+    ...car,
+    pos: newPos,
+    velocity: newVel,
+    acceleration,
+    segmentIdx: nextSegmentIdx,
+    distanceTraveled: newDistanceTraveled,
+  };
+}
+
+function selectCarTarget(
   trackPoints: Vector2D[],
   car: Car,
 ): {
@@ -83,58 +132,6 @@ export function selectCarTarget(
     target,
     nextSegmentIdx,
     distanceToTarget,
-  };
-}
-
-export function updateCarWithTarget(
-  car: Car,
-  target: Vector2D,
-  distanceToTarget: number,
-  nextSegmentIdx: number,
-): Car {
-  const directionVector = target.subtract(car.pos);
-  const trackDir = directionVector.normalized();
-
-  const velMag = car.velocity.length();
-  const velDir = velMag === 0 ? trackDir : car.velocity.normalized();
-  const alignmentDot = velDir.dot(trackDir);
-  const penalty = Math.max(0, 1 - alignmentDot);
-
-  const BASE_ACCEL = 0.0006;
-  const slowdownDistance = 200 * car.brakeForce;
-  const slowdownFactor =
-    distanceToTarget < slowdownDistance
-      ? distanceToTarget / slowdownDistance
-      : 1;
-
-  const accel =
-    BASE_ACCEL * (car.accelFactor ?? 1) * (1 - 0.5 * penalty) * slowdownFactor;
-
-  const acceleration = trackDir.multiplyScalar(accel);
-
-  const DRAG_K = 0.05 * car.dragFactor;
-  let newVel = car.velocity.add(acceleration);
-
-  const STEERING_GAIN = 0.2;
-  newVel.x += (trackDir.x * velMag - newVel.x) * STEERING_GAIN;
-  newVel.y += (trackDir.y * velMag - newVel.y) * STEERING_GAIN;
-
-  newVel.x -= newVel.x * DRAG_K * penalty;
-  newVel.y -= newVel.y * DRAG_K * penalty;
-
-  newVel = newVel.limitLength(car.maxSpeed);
-
-  const newPos = car.pos.add(newVel);
-  const distanceThisFrame = car.pos.distanceTo(newPos);
-  const newDistanceTraveled = car.distanceTraveled + distanceThisFrame;
-
-  return {
-    ...car,
-    pos: newPos,
-    velocity: newVel,
-    acceleration,
-    segmentIdx: nextSegmentIdx,
-    distanceTraveled: newDistanceTraveled,
   };
 }
 
