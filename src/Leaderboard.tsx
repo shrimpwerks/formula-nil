fb1a12c04dbc194a5055d4f037c456c520d9b379
Jonah George <jonah.george@icloud.com>
2025-08-30 21:21:30 -0700

    Add position to leaderboard, hide debug details

--- src/Leaderboard.tsx
+++ src/Leaderboard.tsx
@@ -8,16 +8,20 @@ export default function ({ cars, race }: { cars: Car[]; race: Race }) {
 
       {cars
         .sort((a, b) => b.distanceTraveled - a.distanceTraveled)
-        .map((car) => (
-          <div className="text-white border-white border-1 p-3" key={car.id}>
-            {car.name} <br />
-            Speed: {(car.velocity.length() * 1000).toFixed(1)} <br />
-            Max Speed: {(car.maxSpeed * 1000).toFixed(1)} <br />
-            Distance: {Math.round(car.distanceTraveled)} <br />
-            Acceleration: {car.accelFactor.toFixed(2)}x <br />
-            Drag: {car.dragFactor.toFixed(2)}x <br />
-            Brake Force: {car.brakeForce.toFixed(2)}x <br />
-          </div>
+        .map((car, index) => (
+          <details className="text-white border-white border-1 p-3" key={car.id}>
+            <summary className="cursor-pointer">
+              #{index + 1} {car.name}
+            </summary>
+            <div className="mt-2 ml-4">
+              Speed: {(car.velocity.length() * 1000).toFixed(1)} <br />
+              Max Speed: {(car.maxSpeed * 1000).toFixed(1)} <br />
+              Distance: {Math.round(car.distanceTraveled)} <br />
+              Acceleration: {car.accelFactor.toFixed(2)}x <br />
+              Drag: {car.dragFactor.toFixed(2)}x <br />
+              Brake Force: {car.brakeForce.toFixed(2)}x <br />
+            </div>
+          </details>
         ))}
     </div>
   );
